d846c224529a62ab25584b48aebf98d660bf3094
giogaf <giogaf@gmail.com>
2016-07-18 16:26:59 -0500

    Added flatly-style from bootswatch.com

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,18 @@
 <html>
     <head>
         <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+<link rel="stylesheet" href="css/app.css">
+{{-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> --}}
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js">
+</script>
     </head>
     <body>
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
             </div>
+            <a href="http://www.wjgilmore.com" class="btn btn-success">W.J. Gilmore, LLC</a>
         </div>
     </body>
 </html>

9158339e91be50b47a0acd89593cb8db6878c5c1
giogaf <giogaf@gmail.com>
2016-07-19 09:38:09 -0500

    Create Tarea sampleData

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use taskapp\Tarea;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $faker = \Faker\Factory::create();
+        foreach (range(1,10) as $index) {
+        	Tarea::create([
+        		'nombre' => $faker->sentence(2),
+        		'descripcion' => $faker->sentence(4),
+        		]);
+        }
     }
 }

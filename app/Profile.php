b716dc12cc3e4f65ae10109b61a31b5b9416184e
giogaf <giogaf@gmail.com>
2016-07-22 08:39:32 -0500

    belongs to relation profile-user

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+	public function user()
+	{
+		return $this->belongsTo('taskapp\User');
+	}
+
     /**
      * Asignación masiva por model::create(Array);
      */
